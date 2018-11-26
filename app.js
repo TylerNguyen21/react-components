4337566196b19e54fb7156c4292ee70b375d28ee
Tyler Nguyen <tydnguyen@ucdavis.edu>
2018-11-26 11:41:33 -0800

    npm install

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-// TODO
+let GroceryList = () => {
+  <h1> Groceries List </h1>
+    <div>
+        <ul>Cheese</ul>
+        <ul>Milk</ul>
+    </div>  
+};
+
+ReactDOM.render(<GroceryList/>, document.getElementbyID('app'));
