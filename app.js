0b0a29aa8f3f26013084392ea3215c5f16f575c3
Tyler Nguyen <tydnguyen@ucdavis.edu>
2018-11-26 22:29:36 -0800

    baremins

--- app.js
+++ app.js
@@ -1,9 +1,49 @@
-let GroceryList = () => {
-  <h1> Groceries List </h1>
-    <div>
-        <ul>Cheese</ul>
-        <ul>Milk</ul>
-    </div>  
-};
+class GroceryItem extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            done: false
+        }
+        this.highlight = {
+            over: false
+        }
+    }
+    onListItemClick() {
+      this.setState({
+          done: !this.state.done
+      });
+    }
 
-ReactDOM.render(<GroceryList/>, document.getElementbyID('app'));
+    highlightOverItem() {
+        this.onItem({
+            over: !this.highlight.over
+        });
+    }
+
+    render() {
+        var style ={
+            textDecoration: this.state.done ? 'line-through' : 'none'
+        }
+        var highlight = {
+            fontWeight: this.highlight.over ? 'bold' : 'none'
+        }
+        return (
+        <li style={style} onClick={this.onListItemClick.bind(this)} onmouseover={this.highlightOverItem.bind(this)}>{this.props.groceries}</li>
+        )    
+    } 
+}
+
+let GroceryList = (props) => (
+    <ul>
+        {props.groceries.map(grocery => <GroceryItem groceries={grocery} />)}
+    </ul>
+)
+
+let App = () => (
+ <div>
+     <h2>To Do List</h2>
+     <GroceryList groceries={['cheese', 'milk']}/>
+ </div>
+)
+
+ReactDOM.render(<App />, document.getElementById("app"));
